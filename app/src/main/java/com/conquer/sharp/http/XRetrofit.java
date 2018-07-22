a8866c720d81457c75e5b81f87e6df8bfcf2f1d4
wanghaihui <465495722@qq.com>
2018-07-23 01:38:14 +0800

    x retrofit

---
+++ app/src/main/java/com/conquer/sharp/http/XRetrofit.java
@@ -0,0 +1,42 @@
+package com.conquer.sharp.http;
+
+import com.conquer.sharp.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class XRetrofit {
+    private static class Holder {
+        static XRetrofit instance = new XRetrofit();
+    }
+
+    private XRetrofit(){
+
+    }
+
+    public static XRetrofit instance() {
+        return XRetrofit.Holder.instance;
+    }
+
+    public Retrofit get() {
+        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        if (BuildConfig.DEBUG) {
+            clientBuilder.addInterceptor(logInterceptor);
+        }
+
+        clientBuilder.addInterceptor(new BaseInterceptor());
+
+        return new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BuildConfig.API_HOST)
+                .client(clientBuilder.build())
+                .build();
+    }
+}
