f5ccd00d3cf04bdf80ebb6c543d9627ab3cc56d8
Wanghaihui <465495722@qq.com>
2018-08-11 19:03:42 +0800

    recycler difficult

---
+++ app/src/main/java/com/conquer/sharp/recycler/difficult/adapter/IRecyclerView.java
@@ -0,0 +1,10 @@
+package com.conquer.sharp.recycler.difficult.adapter;
+
+/**
+ * Created by ac on 18/8/11.
+ *
+ */
+
+public interface IRecyclerView {
+
+}
---
+++ app/src/main/java/com/conquer/sharp/recycler/difficult/holder/BaseViewHolder.java
@@ -0,0 +1,214 @@
+package com.conquer.sharp.recycler.difficult.holder;
+
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+import android.support.v7.widget.RecyclerView;
+import android.text.util.Linkify;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+/**
+ * Created by ac on 18/8/11.
+ *
+ */
+
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+
+    private final SparseArray<View> views;
+
+    private final LinkedHashSet<Integer> childClickViewIds;
+    private final LinkedHashSet<Integer> childLongClickViewIds;
+
+    public View convertView;
+
+    /**
+     * Package private field to retain the associated user object and detect a change
+     */
+    Object associatedObject;
+
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+
+        views = new SparseArray<>();
+        childClickViewIds = new LinkedHashSet<>();
+        childLongClickViewIds = new LinkedHashSet<>();
+        convertView = itemView;
+    }
+
+    public HashSet<Integer> getChildClickViewIds() {
+        return childClickViewIds;
+    }
+
+    public HashSet<Integer> getChildLongClickViewIds() {
+        return childLongClickViewIds;
+    }
+
+    public View getConvertView() {
+        return convertView;
+    }
+
+    public BaseViewHolder setText(int viewId, CharSequence value) {
+        TextView view = getView(viewId);
+        view.setText(value);
+        return this;
+    }
+
+    public BaseViewHolder setText(int viewId, @StringRes int strId) {
+        TextView view = getView(viewId);
+        view.setText(strId);
+        return this;
+    }
+
+    public BaseViewHolder setImageResource(int viewId, @DrawableRes int imageResId) {
+        ImageView view = getView(viewId);
+        view.setImageResource(imageResId);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundColor(int viewId, int color) {
+        View view = getView(viewId);
+        view.setBackgroundColor(color);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundRes(int viewId, @DrawableRes int backgroundRes) {
+        View view = getView(viewId);
+        view.setBackgroundResource(backgroundRes);
+        return this;
+    }
+
+    public BaseViewHolder setTextColor(int viewId, int textColor) {
+        TextView view = getView(viewId);
+        view.setTextColor(textColor);
+        return this;
+    }
+
+    public BaseViewHolder setImageDrawable(int viewId, Drawable drawable) {
+        ImageView view = getView(viewId);
+        view.setImageDrawable(drawable);
+        return this;
+    }
+
+    public BaseViewHolder setImageBitmap(int viewId, Bitmap bitmap) {
+        ImageView view = getView(viewId);
+        view.setImageBitmap(bitmap);
+        return this;
+    }
+
+    public BaseViewHolder setAlpha(int viewId, float value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getView(viewId).setAlpha(value);
+        } else {
+            // Pre-honeycomb hack to set Alpha value
+            AlphaAnimation alphaAnimation = new AlphaAnimation(value, value);
+            alphaAnimation.setDuration(0);
+            alphaAnimation.setFillAfter(true);
+            getView(viewId).startAnimation(alphaAnimation);
+        }
+        return this;
+    }
+
+    public BaseViewHolder setVisible(int viewId, boolean visible) {
+        View view = getView(viewId);
+        view.setVisibility(visible ? View.VISIBLE : View.GONE);
+        return this;
+    }
+
+    /**
+     * 链接
+     */
+    public BaseViewHolder linkify(int viewId) {
+        TextView view = getView(viewId);
+        Linkify.addLinks(view, Linkify.ALL);
+        return this;
+    }
+
+    public BaseViewHolder setTypeface(int viewId, Typeface typeface) {
+        TextView view = getView(viewId);
+        view.setTypeface(typeface);
+        view.setPaintFlags(view.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+        return this;
+    }
+
+    public BaseViewHolder setTypeface(Typeface typeface, int... viewIds) {
+        for (int viewId : viewIds) {
+            TextView view = getView(viewId);
+            view.setTypeface(typeface);
+            view.setPaintFlags(view.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+        }
+        return this;
+    }
+
+    public BaseViewHolder setProgress(int viewId, int progress) {
+        ProgressBar view = getView(viewId);
+        view.setProgress(progress);
+        return this;
+    }
+
+    public BaseViewHolder setProgress(int viewId, int progress, int max) {
+        ProgressBar view = getView(viewId);
+        view.setMax(max);
+        view.setProgress(progress);
+        return this;
+    }
+
+    public BaseViewHolder setMax(int viewId, int max) {
+        ProgressBar view = getView(viewId);
+        view.setMax(max);
+        return this;
+    }
+
+    public BaseViewHolder setRating(int viewId, float rating) {
+        RatingBar view = getView(viewId);
+        view.setRating(rating);
+        return this;
+    }
+
+    public BaseViewHolder setRating(int viewId, float rating, int max) {
+        RatingBar view = getView(viewId);
+        view.setMax(max);
+        view.setRating(rating);
+        return this;
+    }
+
+    public BaseViewHolder addOnClickListener(int viewId) {
+        childClickViewIds.add(viewId);
+        return this;
+    }
+
+    public BaseViewHolder addOnLongClickListener(int viewId) {
+        childLongClickViewIds.add(viewId);
+        return this;
+    }
+
+    public BaseViewHolder setOnTouchListener(int viewId, View.OnTouchListener listener) {
+        View view = getView(viewId);
+        view.setOnTouchListener(listener);
+        return this;
+    }
+
+
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView(int viewId) {
+        View view = views.get(viewId);
+        if (view == null) {
+            view = convertView.findViewById(viewId);
+            views.put(viewId, view);
+        }
+        return (T) view;
+    }
+}
