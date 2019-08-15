ed2d9298a71f0ca26c3ffcb8a2bbef6205d8014a
wanghaihui <465495722@qq.com>
2019-08-15 12:05:40 +0800

    selector selected

---
+++ app/src/main/java/com/conquer/sharp/business/SelectorActivity.java
@@ -0,0 +1,42 @@
+package com.conquer.sharp.business;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.conquer.sharp.R;
+import com.conquer.sharp.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SelectorActivity extends BaseActivity {
+
+    @BindView(R.id.funcUseAudio)
+    ImageButton funcUseAudio;
+
+    public static void newInstance(Context context) {
+        Intent intent = new Intent(context, SelectorActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_selector);
+        ButterKnife.bind(this);
+
+        funcUseAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (funcUseAudio.isSelected()) {
+                    funcUseAudio.setSelected(false);
+                } else {
+                    funcUseAudio.setSelected(true);
+                }
+            }
+        });
+    }
+
+}
